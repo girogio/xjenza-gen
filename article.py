9a74d943fb2d3b2a92e025740149ecbd9c2b5261
girogio <giorgiogrigolo03@gmail.com>
2024-01-26 02:19:38 +0100

    Formatted the code

--- article.py
+++ article.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Author:
     name: str
     email: str
@@ -5,7 +8,9 @@ class Author:
     affiliation: str
     is_corresponding: bool
 
-    def __init__(self, name: str = '', surname: str = '', email: str = '', affiliation: str = ''):
+    def __init__(
+        self, name: str = "", surname: str = "", email: str = "", affiliation: str = ""
+    ):
         self.name = name
         self.surname = surname
         self.email = email
@@ -15,25 +20,30 @@ class Author:
 
 class Article:
     title: str
+    year: int
     authors: list[Author]
 
-    def __init__(self, title: str = '', authors: list[Author] = []):
+    def __init__(
+        self,
+        title: str = "",
+        year: int = datetime.datetime.now().year,
+        authors: list[Author] = [],
+    ):
         self.title = title
         self.authors = authors
+        self.year = year
 
     def dict(self):
-
-        # Number the authors and
-        authors_string = ''
+        authors_string = ""
         for i, author in enumerate(self.authors):
             authors_string += f"{author.name[0]}. {author.surname}$^{{{i+1}}}$"
             if i == len(self.authors) - 2:
-                authors_string += ' and '
+                authors_string += " and "
             elif i != len(self.authors) - 1:
-                authors_string += ', '
+                authors_string += ", "
 
         # if any affiliation is the same, group them together
-        affiliation_string = ''
+        affiliation_string = ""
         for i, author in enumerate(self.authors):
             if author.affiliation in [a.affiliation for a in self.authors[:i]]:
                 continue
@@ -43,17 +53,21 @@ class Article:
         for i, author in enumerate(self.authors):
             for j, other_author in enumerate(self.authors[:i]):
                 if author.affiliation == other_author.affiliation:
-                    authors_string = authors_string.replace(f"$^{{{i+1}}}$", f"$^{{{j+1}}}$")
+                    authors_string = authors_string.replace(
+                        f"$^{{{i+1}}}$", f"$^{{{j+1}}}$"
+                    )
                     break
 
         corresponder = [author for author in self.authors if author.is_corresponding]
         corresponder = corresponder[0] if len(corresponder) > 0 else None
 
         dict_to_return = {
-            'title': self.title,
-            'authors': authors_string,
-            'affiliations': affiliation_string,
-            'corresponder': corresponder
+            "title": self.title,
+            "year": self.year,
+            "authors": self.authors,
+            "authors_string": authors_string,
+            "affiliations": affiliation_string,
+            "corresponder": corresponder,
         }
 
         return dict_to_return
