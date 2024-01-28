ac4154d857283cc17ea74c31dc82a91994f69374
girogio <giorgiogrigolo03@gmail.com>
2024-01-28 19:18:23 +0100

    Colourful prompts in data entry

--- xjenza_gen/prompts.py
+++ xjenza_gen/prompts.py
@@ -1,67 +1,104 @@
 from datetime import datetime
 
-from typer import confirm, prompt
+from rich import print
+from rich.prompt import Confirm, IntPrompt, Prompt
+from rich.table import Table
+from typer import clear
 
 from .article import Article, Author
 
 
 def prompt_title() -> str:
     """Prompt the user for the title of the article."""
-    return prompt("Title of the article?", default="A very interesting article")
+    return Prompt.ask(
+        "[blue]Title of the article", default="A very interesting article"
+    )
 
 
 def prompt_short_title(title: str) -> str:
     """Prompt the user for the short title of the article."""
-    return prompt(
-        "Short title of the article?",
+    return Prompt.ask(
+        "[blue]Short title of the article?",
         default=title if len(title) < 150 else "",
-        type=str,
     )
 
 
 def prompt_year_of_publication() -> int:
     """Prompt the user for the year of publication of the article."""
-    return prompt("Year of publication?", default=datetime.now().year, type=int)
+    return IntPrompt.ask("[blue]Year of publication", default=datetime.now().year)
 
 
 def prompt_authors() -> list[Author]:
     """Prompt the user for the authors of the article."""
     authors = []
     while True:
-        print(f"\n{len(authors)} author(s) so far\n{'-' * 20}\n")
+        clear()
 
-        full_name: str = prompt(
-            "Author's full name? (leave empty to exit)",
+        table = Table(title="Authors")
+        table.add_column("Name")
+        table.add_column("Surname")
+        table.add_column("Email")
+        table.add_column("Affiliation")
+        table.add_column("Corresponding")
+
+        for author in authors:
+            table.add_row(
+                author.name,
+                author.surname,
+                author.email,
+                author.affiliation,
+                "Yes" if author.is_corresponding else "No",
+            )
+
+        print(table)
+
+        full_name: str = Prompt.ask(
+            "[blue]Author's full name (leave blank to cancel)",
             default="",
             show_default=False,
-            type=str,
         )
 
-        # Stop if the user leaves the prompt empty
+        # If the user left the field blank, stop asking for authors
         if not full_name:
             break
 
+        # Split the full name into possible first, middle and last names
         name_words = full_name.strip().split(" ")
+
+        # If the user only entered one word, try again
         if len(name_words) < 2:
             print("ERROR: Please enter the author's full name")
             continue
-        else:  # We have a first name and a surname or more
+        else:  # We have at least two words and the surname is the last one
             surname = name_words[-1]
             name = " ".join(name_words[:-1])
 
-        email: str = prompt("Author's email?")
-        affiliation: str = prompt("Author's affiliation?")
-        is_corresponding: bool = confirm(f"Is {full_name} the corresponding author?")
+        email = Prompt.ask("[blue]Author's email")
+        affiliation = Prompt.ask("[blue]Author's affiliation")
+
+        is_corresponding = Confirm.ask(
+            f"[blue]Is {full_name} the corresponding author?",
+            default=(
+                True
+                if len(authors) == 0
+                and not any([author.is_corresponding for author in authors])
+                else False
+            ),
+        )
+
         author = Author(name, surname, email, affiliation, is_corresponding)
 
         authors.append(author)
+
+        if not Confirm.ask("[blue]Add another author?", default=True):
+            break
 
     return authors
 
 
 def prompt_abstract() -> str:
     """Prompt the user for the abstract of the article."""
-    abstract = prompt("Abstract of the article?", default="", type=str)
+    abstract = Prompt.ask("[blue]Abstract of the article?", default="")
     word_count = len(abstract.split(" "))
     if word_count >= 250:
         print(
@@ -73,7 +110,7 @@ def prompt_abstract() -> str:
 
 def prompt_keywords() -> list[str]:
     """Prompt the user for the keywords of the article."""
-    keywords = prompt("Enter a comma-separated list of keywords", default="", type=str)
+    keywords = Prompt.ask("Enter a comma-separated list of keywords", default="")
     return [keyword.strip() for keyword in keywords.split(",")]
 
 
